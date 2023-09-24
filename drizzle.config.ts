1b8a2f1cc25c5a131481cae09719e6d50d354ae7
Hajbo <hajbodev@gmail.com>
2023-09-24 22:52:20 +0200

    Move credentials to an env file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,10 @@
 import type { Config } from "drizzle-kit";
+import { dbCredentials } from "./src/config";
 
 export default {
     out: "./src/db/migrations",
     schema: "./src/db/schemas/*.ts",
     breakpoints: false,
     driver: "pg",
-    dbCredentials: {
-        host: "0.0.0.0",
-        port: 5432,
-        user: "postgres",
-        password: "postgres",
-        database: "medium"
-    }
+    dbCredentials: dbCredentials
 } satisfies Config;
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,13 @@
 import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
- 
+
+import { dbCredentialsString } from '../config';
+
 // for migrations
-const migrationClient = postgres("postgres://postgres:postgres@0.0.0.0:5432/medium", { max: 1 });
+const migrationClient = postgres(dbCredentialsString, { max: 1 });
 migrate(drizzle(migrationClient), {migrationsFolder: './migrations'})
- 
+
 // for query purposes
-const queryClient = postgres("postgres://postgres:postgres@0.0.0.0:5432/medium");
+const queryClient = postgres(dbCredentialsString);
 export const db: PostgresJsDatabase = drizzle(queryClient);
