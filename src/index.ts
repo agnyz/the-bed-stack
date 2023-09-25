4daee07ce37de35a30e580235be26573ee1d1bc2
yamyam263 <yamyam263@gmail.com>
2023-09-25 16:38:48 +0300

    feat: use decorators

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Elysia } from "elysia";
 import { UsersService} from "./users/users.service";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .decorate("usersService" , new UsersService())
+    .get("/", () => "Hello Elysia")
+    .get("/users", async ({ usersService }) => {
+        return usersService.findAll();
+    })
+    .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
-const usersService = new UsersService();
-console.log(await usersService.findAll());
