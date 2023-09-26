6233af324748fefa0646a720d69bc59451ad594f
yamyam263 <yamyam263@gmail.com>
2023-09-26 21:05:05 +0300

    feat: support bun-like .env hierarchy in docker compose

---
+++ scripts/create-start-container-with-env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# this script extends docker-compose up by supporting .env files in the same way as bun
+# see: https://bun.sh/docs/runtime/env#setting-environment-variables
+
+# Default env file
+env_file=".env"
+
+# Determine which env file to use based on NODE_ENV
+if [ "$NODE_ENV" == "production" ]; then
+  env_file=".env.production"
+elif [ "$NODE_ENV" == "development" ]; then
+  env_file=".env.development"
+elif [ "$NODE_ENV" == "test" ]; then
+  env_file=".env.test"
+fi
+
+# If .env.local exists, use that instead
+if [ -f ".env.local" ]; then
+  env_file=".env.local"
+fi
+
+# Run docker-compose with the selected env file
+docker-compose --env-file $env_file up -d
