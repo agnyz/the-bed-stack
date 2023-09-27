cea711614ed287f7e1e26bcbb6cad423f92020eb
yamyam263 <yamyam263@gmail.com>
2023-09-28 01:40:11 +0300

    refactor: remove redundant line

--- src/users/users.plugin.ts
+++ src/users/users.plugin.ts
@@ -1,18 +1,15 @@
 import { Elysia } from 'elysia';
 import { setupUsers } from '@/users/users.module';
 
-export const usersPlugin = new Elysia()
-  .use(setupUsers)
-  .model({})
-  .group(
-    '/users',
-    {
-      detail: {
-        tags: ['Users'],
-      },
+export const usersPlugin = new Elysia().use(setupUsers).group(
+  '/users',
+  {
+    detail: {
+      tags: ['Users'],
     },
-    (app) =>
-      app
-        .post('', ({ store }) => store.usersService.findAll())
-        .post('/login', ({ store }) => store.usersService.findAll()),
-  );
+  },
+  (app) =>
+    app
+      .post('', ({ store }) => store.usersService.findAll())
+      .post('/login', ({ store }) => store.usersService.findAll()),
+);
