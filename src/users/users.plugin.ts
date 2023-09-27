6711a673ecdf4289710247e8aec3822fd920c918
Hajbo <hajbodev@gmail.com>
2023-09-27 23:56:33 +0200

    Update user controller and add endpoints

--- src/users/users.plugin.ts
+++ src/users/users.plugin.ts
@@ -1,15 +1,24 @@
-import { Elysia } from 'elysia';
-import { setupUsers } from '@/users/users.module';
+import { Elysia } from "elysia";
+import { setupUsers } from "@/users/users.module";
+import {
+  InsertUserSchema,
+  UserAuthSchema,
+  UserLoginSchema,
+} from "@/users/users.schema";
 
-export const usersPlugin = new Elysia().use(setupUsers).group(
-  '/users',
-  {
-    detail: {
-      tags: ['Users'],
-    },
-  },
-  (app) =>
-    app
-      .post('', ({ store }) => store.usersService.findAll())
-      .post('/login', ({ store }) => store.usersService.findAll()),
+export const usersPlugin = new Elysia().use(setupUsers).group("/users", (app) =>
+  app
+    .post("", ({ body, store }) => store.usersService.createUser(body.user), {
+      body: InsertUserSchema,
+      response: UserAuthSchema,
+    })
+    .post(
+      "/login",
+      ({ body, store }) =>
+        store.usersService.loginUser(body.user.email, body.user.password),
+      {
+        body: UserLoginSchema,
+        response: UserAuthSchema,
+      }
+    )
 );
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,48 @@
-import { UsersRepository } from '@/users/users.repository';
+// users.service.ts
+// in charge of business logic - generate slug, fetch data from other services, cache something, etc.
+import { NotFoundError } from "elysia";
+import { UsersRepository } from "@/users/users.repository";
+import { UserToCreate } from "@/users/users.schema";
+import { generateToken } from "@/auth";
+import { AuthenticationError } from "@/errors";
 
 export class UsersService {
   constructor(private readonly repository: UsersRepository) {}
 
   async findAll() {
     return this.repository.findAll();
+  }
+
+  async createUser(user: UserToCreate) {
+    user.password = await Bun.password.hash(user.password);
+    const newUser = await this.repository.createUser(user);
+    return {
+      user: {
+        email: newUser.email,
+        bio: newUser.bio,
+        image: newUser.image,
+        username: newUser.username,
+        token: await generateToken(newUser),
+      },
+    };
+  }
+
+  async loginUser(email: string, password: string) {
+    const user = await this.repository.findByEmail(email);
+    if (!user) {
+      throw new NotFoundError("User not found");
+    }
+    if (!(await Bun.password.verify(password, user.password))) {
+      throw new AuthenticationError("Invalid password");
+    }
+    return {
+      user: {
+        email: user.email,
+        bio: user.bio,
+        image: user.image,
+        username: user.username,
+        token: await generateToken(user),
+      },
+    };
   }
 }
