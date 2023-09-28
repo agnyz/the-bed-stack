568a7a5cc0b61d81ce6a76662162db8c567c3fb6
Hajbo <hajbodev@gmail.com>
2023-09-28 21:49:04 +0200

    Bring back db seed and migration

--- db/config.ts
+++ db/config.ts
@@ -1,18 +1,18 @@
 import type { Config } from 'drizzle-kit';
 export const dbCredentials = {
-    host: Bun.env.POSTGRES_HOST || "0.0.0.0",
-    port: parseInt(Bun.env.POSTGRES_PORT || '5432'),
-    user: Bun.env.POSTGRES_USER || "postgres",
-    password: Bun.env.POSTGRES_PASSWORD || "postgres",
-    database: Bun.env.POSTGRES_DB || "medium"
-}
+  host: process.env.POSTGRES_HOST || '0.0.0.0',
+  port: parseInt(process.env.POSTGRES_PORT || '5432'),
+  user: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'medium',
+};
 
 export const dbCredentialsString = `postgres://${dbCredentials.user}:${dbCredentials.password}@${dbCredentials.host}:${dbCredentials.port}/${dbCredentials.database}`;
 
 export default {
-    out: "./migrations",
-    schema: "**/*.schema.ts",
-    breakpoints: false,
-    driver: "pg",
-    dbCredentials
+  out: './db/migrations',
+  schema: '**/*.schema.ts',
+  breakpoints: false,
+  driver: 'pg',
+  dbCredentials,
 } satisfies Config;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
-import { Elysia } from "elysia";
-import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { setupApp } from "@/app.module";
-import { migrationsClient } from "@/database.providers";
-
-
-await migrate(drizzle(migrationsClient), {
-  migrationsFolder: `${import.meta.dir}/../db/migrations`,
-});
+import { Elysia } from 'elysia';
+import { setupApp } from '@/app.module';
 
 const app = new Elysia({ prefix: '/api' }).use(setupApp).listen(3000);
 
