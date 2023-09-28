37999501949ae8977d3f7e650878073bb372f8a6
Hajbo <hajbodev@gmail.com>
2023-09-28 21:49:41 +0200

    Refactor error code handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
-import { Elysia } from "elysia";
-import { swagger } from "@elysiajs/swagger";
-import { jwt } from "@elysiajs/jwt";
-import { title, version, description } from "../package.json";
-import { usersPlugin } from "@/users/users.plugin";
-import { AuthenticationError, AuthorizationError } from "@/errors";
-import { ALG } from "@/auth";
-import { env } from "@/config";
+import { Elysia } from 'elysia';
+import { swagger } from '@elysiajs/swagger';
+import { jwt } from '@elysiajs/jwt';
+import { title, version, description } from '../package.json';
+import { usersPlugin } from '@/users/users.plugin';
+import {
+  AuthenticationError,
+  AuthorizationError,
+  ERROR_CODE_STATUS_MAP,
+} from '@/errors';
+import { ALG } from '@/auth';
+import { env } from '@/config';
 
 // the file name is in the spirit of NestJS, where app module is the device in charge of putting together all the pieces of the app
 // see: https://docs.nestjs.com/modules
-
 
 /**
  * Add all plugins to the app
@@ -21,18 +24,8 @@ export const setupApp = () => {
       AUTHORIZATION: AuthorizationError,
     })
     .onError(({ error, code, set }) => {
-      // Handle Elysia schema validation errors
-      switch (code) {
-        case "VALIDATION":
-          set.status = 422;
-        case "NOT_FOUND":
-          set.status = 404;
-        case "AUTHENTICATION":
-          set.status = 401;
-        case "AUTHORIZATION":
-          set.status = 403;
-      }
-      const errorType = "type" in error ? error.type : "internal";
+      set.status = ERROR_CODE_STATUS_MAP[code];
+      const errorType = 'type' in error ? error.type : 'internal';
       return { errors: { [errorType]: error.message } };
     })
     .use(
@@ -44,10 +37,10 @@ export const setupApp = () => {
     )
     .use(
       jwt({
-        name: "jwt",
+        name: 'jwt',
         secret: env.JWT_SECRET,
         alg: ALG,
-      })
+      }),
     )
-    .group("/api", (app) => app.use(usersPlugin));
+    .group('/api', (app) => app.use(usersPlugin));
 };
--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,6 @@
 export class AuthenticationError extends Error {
   public status = 401;
-  public type = "authentication";
+  public type = 'authentication';
   constructor(public message: string) {
     super(message);
   }
@@ -8,8 +8,20 @@ export class AuthenticationError extends Error {
 
 export class AuthorizationError extends Error {
   public status = 403;
-  public type = "authorization";
+  public type = 'authorization';
   constructor(public message: string) {
     super(message);
   }
 }
+
+export const ERROR_CODE_STATUS_MAP = {
+  PARSE: 400,
+  VALIDATION: 422,
+  NOT_FOUND: 404,
+  INVALID_COOKIE_SIGNATURE: 401,
+  AUTHENTICATION: 401,
+  AUTHORIZATION: 403,
+  INTERNAL_SERVER_ERROR: 500,
+  UNKNOWN: 500,
+  DEFAULT: 500,
+};
