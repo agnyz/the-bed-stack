b8b668939cc684f70a70a11c1ed78ce6283da7fa
yamyam263 <yamyam263@gmail.com>
2023-10-08 22:11:05 +0000

    feat: generalize table drop

--- db/drop.ts
+++ db/drop.ts
@@ -1,7 +1,22 @@
 import { db } from '@/database.providers';
 import { users } from '@/users/users.schema';
-// TODO: add commander
+import { getTableName } from 'drizzle-orm';
+import { exit } from 'process';
+// TODO: use react-ink
+
+const tables = [users];
+
 console.log('Dropping the entire database');
-db.delete(users);
-// add more tables here
+
+for (const table of tables) {
+  const name = getTableName(table);
+  console.log(`Dropping ${name}`);
+  await db.delete(users);
+  console.log(`Dropped ${name}`);
+  const tableResult = await db.select().from(table);
+  console.log(`${name} result: `, tableResult);
+}
+
 console.log('Database dropped');
+
+exit(0);
