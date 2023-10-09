c29ea00a540410a2e3cc2278b8c36a9fcdce47b5
Yam Borodetsky <yamyam263@gmail.com>
2023-10-09 10:47:01 +0000

    fix: create-db correctly call wait-for-db script

--- scripts/create-db.sh
+++ scripts/create-db.sh
@@ -4,4 +4,4 @@
 source .env
 
 # Run wait-for-db.sh with the sourced environment variables
-./scripts/wait-for-db.sh db:5432 -- strict --timeout=60 -- psql -h db -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c 'SELECT 1;' && bun db:migrate && bun db:seed
+./scripts/wait-for-db.sh db:5432 --strict --timeout=60 -- psql -h db -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c 'SELECT 1;' && bun db:migrate && bun db:seed
