6099902fd7f89c3f1abdab87cafe7e439094e4f5
Yam Borodetsky <yamyam263@gmail.com>
2023-10-09 10:40:38 +0000

    fix: create-db.sh reference correct script

--- scripts/create-db.sh
+++ scripts/create-db.sh
@@ -4,4 +4,4 @@
 source .env
 
 # Run wait-for-it.sh with the sourced environment variables
-./wait-for-it.sh db:5432 -- strict --timeout=60 -- psql -h db -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c 'SELECT 1;' && bun db:migrate && bun db:seed
+./scripts/wait-for-it.sh db:5432 -- strict --timeout=60 -- psql -h db -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c 'SELECT 1;' && bun db:migrate && bun db:seed
