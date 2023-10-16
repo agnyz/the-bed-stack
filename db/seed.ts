05e49e84960213a9d0654745afeb0193c6eac73d
aruay <aruay@starfund.app>
2023-10-17 00:25:06 +0600

    feat: data faker

--- db/seed.ts
+++ db/seed.ts
@@ -1,19 +1,21 @@
 import { exit } from 'process';
 import { db } from '@/database.providers';
 import { users } from '@/users/users.model';
+import { faker } from '@faker-js/faker';
 
-const data = {
-  id: 1, //do not use 'users.id.default', seed should be idempotent
-  email: 'test@email.com',
-  username: 'test',
-  password: 'test',
-  bio: 'test',
-  image: 'test',
-};
-console.log('Upserting user: ', data);
-await db.insert(users).values(data).onConflictDoNothing();
-console.log('User upserted');
-
+for (let i = 0; i < 10; i++) {
+  const data = {
+    id: faker.datatype.number(),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    bio: faker.lorem.text(),
+    image: faker.image.imageUrl(),
+  };
+  console.log('Upserting user:', data);
+  await db.insert(users).values(data).onConflictDoNothing();
+  console.log('User upserted');
+}
 const userResult = await db.select().from(users);
 console.log('User result: ', userResult);
 
