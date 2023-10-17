e7770e6f9581e5cf0a05df6b0702559f3d1fef3a
yamyam263 <yamyam263@gmail.com>
2023-10-17 17:26:23 +0000

    docs: display version

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitepress';
-import { name, title, description } from '../../package.json';
+import { name, title, description, version } from '../../package.json';
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -9,7 +9,22 @@ export default defineConfig({
   description,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [{ text: 'Guide', link: '/what-is-elysiajs-realworld' }],
+    nav: [
+      { text: 'Guide', link: '/what-is-elysiajs-realworld' },
+      {
+        text: `v${version}`,
+        items: [
+          {
+            text: 'Changelog',
+            link: 'https://github.com/agnyz/elysia-realworld-example-app/blob/main/CHANGELOG.md',
+          },
+          {
+            text: 'Contributing',
+            link: 'https://github.com/agnyz/elysia-realworld-example-app/blob/main/CONTRIBUTING.md',
+          },
+        ],
+      },
+    ],
 
     sidebar: [
       {
@@ -21,6 +36,24 @@ export default defineConfig({
           },
           { text: 'Getting Started', link: '/getting-started' },
         ],
+        collapsed: false,
+      },
+      {
+        text: 'Experimental',
+        items: [
+          {
+            text: 'Developing in a Dev Container',
+            link: '/dev-container',
+          },
+        ],
+      },
+      {
+        text: 'Contributing',
+        link: 'https://github.com/agnyz/elysia-realworld-example-app/blob/main/CONTRIBUTING.md',
+      },
+      {
+        text: 'Support',
+        link: 'https://github.com/agnyz/elysia-realworld-example-app/blob/main/SUPPORT.md',
       },
     ],
 
