cbb1a51939c17783b8f6fa2ed3b8295258791bbc
Hajbo <hajbodev@gmail.com>
2025-03-11 22:56:04 +0700

    fix: database config export

--- db/config.ts
+++ db/config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 
 export const dbCredentials = {
   host: process.env.POSTGRES_HOST || '0.0.0.0',
-  port: parseInt(process.env.POSTGRES_PORT || '5432'),
+  port: Number.parseInt(process.env.POSTGRES_PORT || '5432'),
   user: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: process.env.POSTGRES_DB || 'medium',
@@ -15,6 +15,5 @@ export default {
   // in our codebase, 'schema' refers to TypeBox objects (abstraction of a JSON schema), while 'model' refers to database entities/tables, which is what drizzle refers to as 'schema'
   schema: '**/*.model.ts',
   breakpoints: false,
-  driver: 'pg',
-  dbCredentials,
+  dialect: 'postgresql',
 } satisfies Config;
