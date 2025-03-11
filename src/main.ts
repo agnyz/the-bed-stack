c6eecd0c86cb463dfd310688af11870d3c8cf46e
Hajbo <hajbodev@gmail.com>
2025-03-11 22:55:36 +0700

    fix: redirect usage

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { setupApp } from '@/app.module';
 
 const app = new Elysia()
   .use(setupApp)
-  .get('/', ({ set }) => {
-    set.redirect = '/swagger';
-  })
+  .get('/', ({ redirect }) => redirect('/swagger'))
   .listen(3000);
 
 console.log(
