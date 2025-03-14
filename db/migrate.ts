2f2d891e9a7bc43e95f55d4da38b22ddb8214996
Hajbo <hajbodev@gmail.com>
2025-03-14 21:13:48 +0700

    chore: upgrade migrations and fix broken code

--- db/migrate.ts
+++
@@ -1,9 +0,0 @@
-import { exit } from 'node:process';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import { migrationsClient } from '@/database.providers';
-
-await migrate(drizzle(migrationsClient), {
-  migrationsFolder: `${import.meta.dir}/migrations`,
-});
-exit(0);
--- db/seed.ts
+++ db/seed.ts
@@ -7,9 +7,11 @@ console.log('Truncating the user database');
 await db.delete(users);
 console.log('The database is empty: ', await db.select().from(users));
 
+// TODO: consider using drizzle seed
+// https://orm.drizzle.team/docs/seed-overview
 for (let i = 0; i < 10; i++) {
   const data = {
-    id: faker.number.int(),
+    id: faker.number.int({ min: 1, max: 2147483647 }),
     email: faker.internet.email(),
     username: faker.internet.username(),
     password: await Bun.password.hash(faker.internet.password()),
